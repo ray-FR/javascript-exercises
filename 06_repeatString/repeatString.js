46cd1ca00f76b1c99d1204e44f37f67a9fa061a7
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-06-27 16:01:53 +0200

    finish exercice 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(S, n) {
+    if (n < 0){
+        return 'ERROR';
+    }
+    else{
+        let reps = "";
+        for (let i = 0; i < n; i++){
+            reps = reps+S;
+        }
+        return reps;
+    }
 };
 
 // Do not edit below this line
