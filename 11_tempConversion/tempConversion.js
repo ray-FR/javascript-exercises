354084b35816716bbc52e967513cb903eb0e222e
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-06-28 22:52:28 +0200

    finish 10-12

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let a = (num - 32) * 5/9;
+  if (!(Number.isInteger(a))){
+    return parseFloat(a.toFixed(1));}
+  return a;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let a = (num * 9/5) + 32;
+  if (!(Number.isInteger(a))){
+    return parseFloat(a.toFixed(1));}
+  return a;
 };
 
 // Do not edit below this line
--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(l) {
+	let summ = 0;
+  for (let i = 0; i<l.length; i++){
+    summ += l[i]; 
+  }
+  return summ
 };
 
-const multiply = function() {
-
+const multiply = function(l) {
+  let r = 1;
+  for(let i = 0; i < l.length; i++){
+    r*=l[i];
+  }
+  return r;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let f = 1
+  for (let i = 1; i <= n; i++){
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
