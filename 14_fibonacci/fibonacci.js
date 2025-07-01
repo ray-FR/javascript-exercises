61c748813ff589f2494fac27ab27572b088c69ad
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-07-01 14:31:25 +0200

    finish exercices

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0){
+        return 'OOPS';
+    }
 
+    if (num == 0){
+        return 0;
+    }
+    let n1 = 1;
+    let n2 = 1;
+    let tmp;
+    for (let i = 2; i < num; i++){
+        tmp = n2;
+        n2 += n1;
+        n1 = tmp;
+    }
+    return n2
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let oln;
+    let olda = 0;
+    let tmp;
+    for (p in people){
+        people[p].yearOfDeath === undefined ? tmp = (2025 - people[p].yearOfBirth) : tmp = (people[p].yearOfDeath - people[p].yearOfBirth); 
+        if (tmp > olda){
+            oln = people[p];
+            olda = tmp;
+        }
+    }
+
+    return oln;
+
 
 };
 
